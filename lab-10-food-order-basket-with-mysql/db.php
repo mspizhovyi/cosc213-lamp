e794b3371fb98991c5ecab0520f5585f7c8bc6b6
mspizhovyi <max.spizhovyi@myokanagan.bc.ca>
2025-11-10 16:34:33 -0800

    lab 10 completed

--- lab-10-food-order-basket-with-mysql/db.php
+++ lab-10-food-order-basket-with-mysql/db.php
@@ -4,7 +4,7 @@ const DB_HOST = '127.0.0.1';
 const DB_PORT = 3306;
 const DB_NAME = 'food_basket_db';
 const DB_USER = 'maksym_spizhovyi';
-const DB_PASS = 'Max@132006'; 
+const DB_PASS = 'mAxSpIzHoVyI@34513'; 
 
 function get_pdo(): PDO {
     $dsn = 'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';charset=utf8mb4';
